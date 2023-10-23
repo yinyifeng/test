a6e071cefe22154b051cd966068bee6f0e10c4c3
Yinyi <33323282+yinyifeng@users.noreply.github.com>
2023-10-23 17:46:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -85,99 +85,53 @@ if app_mode == 'Introduction':
 
 
     st.markdown("### 00 - Show  Dataset")
-    if select_dataset == "Delivery Time":
-        col1, col2, col3,col4,col5,col6,col7,col8,col9,col10 = st.columns(10)
-        col1.markdown(" **fixed acidity** ")
-        col1.markdown("most acids involved with wine or fixed or nonvolatile (do not evaporate readily)")
-        col2.markdown(" **volatile acidity** ")
-        col2.markdown("the amount of acetic acid in wine, which at too high of levels can lead to an unpleasant, vinegar taste")
-        col3.markdown(" **citric acid** ")
-        col3.markdown("found in small quantities, citric acid can add 'freshness' and flavor to wines")
-        col4.markdown(" **residual sugar** ")
-        col4.markdown("the amount of sugar remaining after fermentation stops, it's rare to find wines with less than 1 gram/liter")
-        col5.markdown(" **chlorides** ")
-        col5.markdown("the amount of salt in the wine")
-        col6.markdown(" **free sulfur dioxide** ")
-        col6.markdown("the free form of SO2 exists in equilibrium between molecular SO2 (as a dissolved gas) and bisulfite ion; it prevents ")
-        col7.markdown(" **total sulfur dioxide** ")
-        col7.markdown("amount of free and bound forms of S02; in low concentrations, SO2 is mostly undetectable in wine, but at free SO2 ")
-        col8.markdown(" **density** ")
-        col8.markdown("the density of water is close to that of water depending on the percent alcohol and sugar content")
-        col9.markdown(" **pH** ")
-        col9.markdown("describes how acidic or basic a wine is on a scale from 0 (very acidic) to 14 (very basic); most wines are between 3-4 on the ")
-        col10.markdown(" **sulphates** ")
-        col10.markdown("a wine additive which can contribute to sulfur dioxide gas (S02) levels, wich acts as an antimicrobia")
-    else:
-        col1, col2, col3,col4,col5,col6,col7,col8,col9,col10,col11,col12,col13,col14 = st.columns(14)
-        col1.markdown(" **CRIM** ")
-        col1.markdown("per capita crime rate by town")
-        col2.markdown(" **ZN** ")
-        col2.markdown("proportion of presidential land zoned for lots over 25,000 sq.ft.")
-        col3.markdown(" **CHAS** ")
-        col3.markdown("Charles River dummy variable (= 1 if tract bounds river; 0 otherwise")
-        col4.markdown(" **NOX** ")
-        col4.markdown("nitric oxides concentration (parts per 10 million)")
-        col5.markdown(" **RM** ")
-        col5.markdown("average number of rooms per dwelling")
-        col6.markdown(" **AGE** ")
-        col6.markdown("proportion of owner-occupied units built prior to 1940")
-        col7.markdown(" **DIS** ")
-        col7.markdown("weighted distances to five Boston employment centres")
-        col8.markdown(" **RAD** ")
-        col8.markdown("index of accessibility to radial highways")
-        col9.markdown(" **TAX** ")
-        col9.markdown("full-value property-tax rate per $10,000")
-        col10.markdown(" **PTRATIO** ")
-        col10.markdown("pupil-teacher ratio by town")                        
-        col11.markdown(" **B** ")
-        col11.markdown("1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town")
-        col12.markdown(" **LSTAT** ")
-        col12.markdown("percentage lower status of the population") 
-        col13.markdown(" **MEDV** ")
-        col13.markdown("Median value of owner-occupied homes in $1000's") 
-        col14.markdown(" **INDUS** ")
-        col14.markdown("proportion of non-retail business acres per town") 
-    num = st.number_input('No. of Rows', 5, 10)
-    head = st.radio('View from top (head) or bottom (tail)', ('Head', 'Tail'))
-    if head == 'Head':
-        st.dataframe(df.head(num))
-    else:
-        st.dataframe(df.tail(num))
+    col1, col2, col3, col4, col5, col6, col7, col8, col9, col10 = st.columns(10)
     
-    st.markdown("Number of rows and columns helps us to determine how large the dataset is.")
-    st.text('(Rows,Columns)')
-    st.write(df.shape)
-
-
-    st.markdown("### 01 - Description")
-    st.dataframe(df.describe())
+        # Descriptions for each variable in the dataset
+        # ... [The code here provides descriptions for each wine quality variable]
+    
+        # Allow users to view either the top or bottom rows of the dataset
+        num = st.number_input('No. of Rows', 5, 10)
+        head = st.radio('View from top (head) or bottom (tail)', ('Head', 'Tail'))
+        if head == 'Head':
+            st.dataframe(df.head(num))
+        else:
+            st.dataframe(df.tail(num))
+    
+        # Display the shape (number of rows and columns) of the dataset
+        st.markdown("Number of rows and columns helps us to determine how large the dataset is.")
+        st.text('(Rows,Columns)')
+        st.write(df.shape)
+    
+        st.markdown("### 01 - Description")
+        st.dataframe(df.describe())
 
 
 
     st.markdown("### 02 - Missing Values")
     st.markdown("Missing values are known as null or NaN values. Missing data tends to **introduce bias that leads to misleading results.**")
-    dfnull = df.isnull().sum()/len(df)*100
-    totalmiss = dfnull.sum().round(2)
-    st.write("Percentage of total missing values:",totalmiss)
-    st.write(dfnull)
-    if totalmiss <= 30:
-        st.success("Looks good! as we have less then 30 percent of missing values.")
-    else:
-        st.warning("Poor data quality due to greater than 30 percent of missing value.")
-        st.markdown(" > Theoretically, 25 to 30 percent is the maximum missing values are allowed, there's no hard and fast rule to decide this threshold. It can vary from problem to problem.")
-
-    st.markdown("### 03 - Completeness")
-    st.markdown(" Completeness is defined as the ratio of non-missing values to total records in dataset.") 
-    # st.write("Total data length:", len(df))
-    nonmissing = (df.notnull().sum().round(2))
-    completeness= round(sum(nonmissing)/len(df),2)
-    st.write("Completeness ratio:",completeness)
-    st.write(nonmissing)
-    if completeness >= 0.80:
-        st.success("Looks good! as we have completeness ratio greater than 0.85.")
-           
-    else:
-        st.success("Poor data quality due to low completeness ratio( less than 0.85).")
+        dfnull = df.isnull().sum()/len(df)*100
+        totalmiss = dfnull.sum().round(2)
+        st.write("Percentage of total missing values:",totalmiss)
+        st.write(dfnull)
+        if totalmiss <= 30:
+            st.success("Looks good! as we have less then 30 percent of missing values.")
+        else:
+            st.warning("Poor data quality due to greater than 30 percent of missing value.")
+            st.markdown(" > Theoretically, 25 to 30 percent is the maximum missing values are allowed, there's no hard and fast rule to decide this threshold. It can vary from problem to problem.")
+    
+        st.markdown("### 03 - Completeness")
+        st.markdown(" Completeness is defined as the ratio of non-missing values to total records in dataset.")
+        # st.write("Total data length:", len(df))
+        nonmissing = (df.notnull().sum().round(2))
+        completeness= round(sum(nonmissing)/len(df),2)
+        st.write("Completeness ratio:",completeness)
+        st.write(nonmissing)
+        if completeness >= 0.80:
+            st.success("Looks good! as we have completeness ratio greater than 0.85.")
+    
+        else:
+            st.success("Poor data quality due to low completeness ratio( less than 0.85).")
 
     st.markdown("### 04 - Complete Report")
     if st.button("Generate Report"):
