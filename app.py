739b1f90fb5afc1ad09368d34b3c7be7f0242116
Yinyi <33323282+yinyifeng@users.noreply.github.com>
2023-10-24 19:44:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,34 +30,6 @@ st.set_page_config(
 
 
 def main():
-    def _max_width_():
-        max_width_str = f"max-width: 1000px;"
-        st.markdown(
-            f"""
-        <style>
-        .reportview-container .main .block-container{{
-            {max_width_str}
-        }}
-        </style>
-        """,
-            unsafe_allow_html=True,
-        )
-
-
-    # Hide the Streamlit header and footer
-    def hide_header_footer():
-        hide_streamlit_style = """
-                    <style>
-                    footer {visibility: hidden;}
-                    </style>
-                    """
-        st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
-    # increases the width of the text and tables/figures
-    _max_width_()
-
-    # hide the footer
-    hide_header_footer()
 
     image_nyu = Image.open('images/nyu.png')
     st.image(image_nyu, width=100)
@@ -234,65 +206,3 @@ def image(src_as_string, **style):
 def link(link, text, **style):
     return a(_href=link, _target="_blank", style=styles(**style))(text)
 
-
-def layout(*args):
-
-    style = """
-    <style>
-      # MainMenu {visibility: hidden;}
-      footer {visibility: hidden;background - color: white}
-     .stApp { bottom: 80px; }
-    </style>
-    """
-    style_div = styles(
-        position="fixed",
-        left=0,
-        bottom=0,
-        margin=px(0, 0, 0, 0),
-        width=percent(100),
-        color="black",
-        text_align="center",
-        height="auto",
-        opacity=1,
-
-    )
-
-    style_hr = styles(
-        display="block",
-        margin=px(8, 8, "auto", "auto"),
-        border_style="inset",
-        border_width=px(2)
-    )
-
-    body = p()
-    foot = div(
-        style=style_div
-    )(
-        hr(
-            style=style_hr
-        ),
-        body
-    )
-
-    st.markdown(style, unsafe_allow_html=True)
-
-    for arg in args:
-        if isinstance(arg, str):
-            body(arg)
-
-        elif isinstance(arg, HtmlElement):
-            body(arg)
-
-    st.markdown(str(foot), unsafe_allow_html=True)
-
-def footer2():
-    myargs = [
-        "👨🏼‍💻 Made by ",
-        link("https://github.com/NYU-DS-4-Everyone", "NYU - Professor Gaëtan Brison"),
-        "🚀"
-    ]
-    layout(*myargs)
-
-
-if __name__ == "__main__":
-    footer2()
